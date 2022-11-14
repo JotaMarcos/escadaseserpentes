8d2eac969504da6d2bfe414e824460df6391a50d
João Marcos <jmarcos_duarte@hotmail.com>
2022-11-13 21:39:39 -0400

    Revalide de configuração

---
+++ lib/src/main/java/escadaseserpentes/Applications.java
@@ -0,0 +1,18 @@
+package escadaseserpentes;
+
+import java.io.Serializable;
+
+// Este é um projeto que fiz para validar conhecimentos sobre a IDE Eclipse
+
+//@SuppressWarnings("serial")
+public class Applications implements Serializable {
+	
+	//public static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Começando apenas uma nova jornada!");
+		System.out.println("O projeto está executando!");
+	}
+
+}
